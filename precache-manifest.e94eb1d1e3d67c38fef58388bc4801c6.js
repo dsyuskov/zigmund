c114d342a753e8e8b7214bab1cc897aaf61c09bf
int_21h <yuskovss@gmail.com>
2020-09-05 11:29:22 +0400

    Updates

---
+++ precache-manifest.e94eb1d1e3d67c38fef58388bc4801c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f99377fd6f9f90290620f80f560c87",
+    "url": "/zigmund/index.html"
+  },
+  {
+    "revision": "9c76460d7ee055dbb99e",
+    "url": "/zigmund/static/css/main.3c4cbdc3.chunk.css"
+  },
+  {
+    "revision": "8263b3238e52007b17c0",
+    "url": "/zigmund/static/js/2.ef238f3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zigmund/static/js/2.ef238f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c76460d7ee055dbb99e",
+    "url": "/zigmund/static/js/main.9bc1a462.chunk.js"
+  },
+  {
+    "revision": "317e647b227a62c6b16c",
+    "url": "/zigmund/static/js/runtime-main.0cb39603.js"
+  }
+]);
